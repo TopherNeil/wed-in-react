0a2d6b5ba61e26caf75de1357dc3784dff008f3d
TopherNeil <gomez.cneil@gmail.com>
2025-05-19 22:52:46 +0800

    Routing setup for authentication

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from '../../components/auth/LoginForm'
+export default function LoginPage() {
+    return (
+        <main className="flex min-h-screen items-center justify-center">
+            <LoginForm />
+        </main>
+    );
+}
---
+++ src/app/components/auth/RegisterForm.tsx
@@ -0,0 +1,7 @@
+export function RegisterForm() {
+    return (
+        <>
+            <p>Register Form Works!</p>
+        </>
+    )
+}
